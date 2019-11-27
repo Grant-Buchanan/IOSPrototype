8cff722880a85d1fba324eea29865c243e225aad
Teylan <grantsrockets@yahoo.com>
2019-11-26 16:43:27 -0800

    Initial

---
+++ Prototype/CustomBackButtonNavController.swift
@@ -0,0 +1,13 @@
+//
+//  CustomBackButtonNavController.swift
+//  Prototype
+//
+//  Created by GrantLBuchanan on 11/21/19.
+//  Copyright © 2019 GrantLBuchanan. All rights reserved.
+//
+
+
+import UIKit
+
+class CustomBackButtonNavController: UINavigationController { }
+
--- Prototype/ViewController.swift
+++ Prototype/ViewController.swift
@@ -13,8 +13,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+    }
+    
+   
+            
+    
     }
 
 
-}
+   
+    
+
+
 
